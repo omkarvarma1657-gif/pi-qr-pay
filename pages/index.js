2106a8d13aff4ba5124207fd3952ca978f54fd55
omkarvarma1657-gif <omkarvarma1657@gmail.com>
2025-08-17 01:02:27 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Pi QR Pay</h1>
+      <p>Welcome to Pi QR Payment App</p>
+    </div>
+  )
+}
