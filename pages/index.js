421bf916fd8c71ce6147c0824bdeaf3c9189eac3
omkarvarma1657-gif <omkarvarma1657@gmail.com>
2025-08-17 14:25:41 +0530

    Update index.js
    fix: add Home component for index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div style={{ 
+      display: "flex", 
+      flexDirection: "column", 
+      alignItems: "center", 
+      justifyContent: "center", 
+      height: "100vh", 
+      fontFamily: "Arial, sans-serif" 
+    }}>
       <h1>Welcome to Pi QR Pay 🚀</h1>
       <p>Scan and Pay easily using Pi Network</p>
     </div>
